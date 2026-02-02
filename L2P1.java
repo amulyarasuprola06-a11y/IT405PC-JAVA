5baad8f2154561e1201e955c4efa2c58bb82f825
amulyarasuprola06-a11y <amulya.rasuprola06@gmail.com>
2026-02-02 23:08:56 +0530

    Add files via upload

---
+++ L2P1.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class L2P1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size:");
+        int n = sc.nextInt();
+
+        int[] a = new int[n];
+
+        System.out.println("Enter the elements into array:");
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        for (int x : a) {
+            if (x <= 1)
+                continue;
+
+            boolean isprime = true;
+
+            for (int j = 2; j <= x / 2; j++) {
+                if (x % j == 0) {
+                    isprime = false;
+                    break;
+                }
+            }
+
+            if (isprime) {
+                System.out.println(x + " ");
+            }
+        }
+    }
+}
