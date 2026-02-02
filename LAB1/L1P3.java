8867bfd5e75d712a7844cc5ab2bcb4963b6b84ec
amulyarasuprola06-a11y <amulya.rasuprola06@gmail.com>
2026-02-02 22:24:41 +0530

    Add files via upload

---
+++ LAB1/L1P3.java
@@ -0,0 +1,14 @@
+  class L1P3{
+       public static void main(String[] args){
+            char ch = 'a';
+            int x = 10;
+            short s = 30;
+            long l = 456;
+             float fl = 30f;
+           double d = x;
+             byte z =12;
+             boolean b = false;
+             System.out.println("Integer value : " + x + "short: "+ s + " long: "+l + "float: " + fl + " double: " +d + "byte: " + z+" boolean: "+ b);
+             
+              }
+         }
