5bff7d5bd4a40e0c2ef8e89e0fb1b55543a46b9a
amulyarasuprola06-a11y <amulya.rasuprola06@gmail.com>
2026-03-09 21:23:08 +0530

    Add files via upload

---
+++ L5P3.java
@@ -0,0 +1,14 @@
+import java.io.*;
+class L5P3{
+     public static void main(String...args) throws IOException{
+     File f= new File("Epson.java");
+
+     if(f.exists()){
+            System.out.println("yes this file exists");
+   }else{
+          f.createNewFile();
+           System.out.println("File Exists: "+f.exists());
+  }
+
+}
+ }
